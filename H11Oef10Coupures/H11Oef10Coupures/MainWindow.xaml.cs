37e9d6fd66e337f54e78da5e324005b95fb4c432
Indy Iri <indy.iri@student.pxl.be>
2024-12-04 23:00:18 +0100

    Afgewerkt

--- H11Oef10Coupures/H11Oef10Coupures/MainWindow.xaml.cs
+++ H11Oef10Coupures/H11Oef10Coupures/MainWindow.xaml.cs
@@ -20,5 +20,57 @@ namespace H11Oef10Coupures
         {
             InitializeComponent();
         }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            moneyTextBox.Clear();
+            resultTextBox.Clear();
+            moneyTextBox.Focus();
+        }
+
+        string inputMoney;
+        double money;
+
+        double[] moneyOptions = new double[] {500.00, 100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01};
+        int[] amountOfMoneyPieces = new int[14];
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+
+            inputMoney = moneyTextBox.Text;
+            bool isInputValid = double.TryParse(inputMoney, out money);
+
+            money = Math.Round(money, 2);
+
+            if (!isInputValid || money < 0)
+            {
+                MessageBox.Show("Voer een geldig bedrag is!","Foutieve invoer", MessageBoxButton.OK, MessageBoxImage.Error);
+                moneyTextBox.Clear();
+                moneyTextBox.Focus();
+            }
+            else
+            {
+                for (int i = 0; i <= moneyOptions.Length - 1; i++)
+                {
+                    amountOfMoneyPieces[i] = (int)(money / moneyOptions[i]);
+                    money -= amountOfMoneyPieces[i] * moneyOptions[i];
+                    money = Math.Round(money, 2);
+                }
+
+                for (int i = 0; i <= moneyOptions.Length - 1; i++)
+                {
+                    if (amountOfMoneyPieces[i] > 0)
+                    {
+                        resultTextBox.Text += $"{amountOfMoneyPieces[i]} stuk(s) van € {moneyOptions[i]:0.00} \n";
+                    }
+                }
+            }   
+        }
     }
 }
